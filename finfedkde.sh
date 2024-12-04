56dfc0c18d191f987f7065729222da231137a084
Zifero <zifero@pm.me>
2024-12-04 15:54:10 +0100

    Update finfedkde.sh

--- finfedkde.sh
+++ finfedkde.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo dnf autoremove -y
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf upgrade -y
 
 removeappfile=$1
@@ -20,4 +21,4 @@ done < "$installappfile"
 wget https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors.x86_64.rpm
 sudo dnf install ./onlyoffice-desktopeditors.x86_64.rpm -y
 
-
+sudo dnf autoremove -y
